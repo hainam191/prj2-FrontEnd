8eb162c70347d8d882bfd105ef2bcb380a4d851a
hainam191 <hainam6991@gmail.com>
2020-06-16 16:51:59 +0700

    crud ok

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Signup from '@/components/Signup'
+import Home from '@/components/Home'
+import BgAdmin from '@/components/BgAdmin'
 
 Vue.use(Router)
 
@@ -10,6 +13,31 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/sign-up',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/home',
+      name: 'LS',
+      component: Home,
+      // children:[{
+      //   path: 'login',
+      //   component: Login,
+      // }]
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: BgAdmin
+    },
+    {
+      path: '*',
+      component: {
+        template: `<h1>404 Not Found</h1>`
+      }
     }
   ],
   mode:'history'
